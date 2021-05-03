7a8da61ada412537f6df0dd52db2f66d0f15ae99
Victoria Kruczek <victoria.kruczek@gmail.com>
2021-05-03 16:20:56 +0200

    Add routes to get all cookies and create a cookie

--- app.js
+++ app.js
@@ -2,17 +2,41 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const morgan = require('morgan')
+const Cookie = require('./models/models')
 
 app.listen(process.env.SERVER_PORT)
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'));
+
+app.get('/all-cookies', (req, res) =>{
+  Cookie.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('allCookies', { cookie: result })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 
 app.get('/', (req, res) => {
   res.render('mainPage')
 })
 
-app.get('/create', (req, res) => {
+app.get('/create-cookie', (req, res) => {
   res.render('create')
+})
+
+app.post('/create-cookie', (req, res) => {
+  const cookie = new Cookie(req.body)
+  cookie.save()
+    .then((result) => {
+      console.log(result)
+      res.redirect('/all-cookies')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 app.use((req, res) => {
---
+++ models/models.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose");
+require('dotenv').config()
+
+const Schema = mongoose.Schema;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(result => console.log("were on!"))
+  .catch(err => console.log(err));
+
+const cookieSchema = new Schema({
+  cookieName: { type: String, required: true },
+  cookieSummary: String,
+  cookieRecepie: [ String ]
+}, {timestamps: true});
+
+const Cookie = mongoose.model("Cookie", cookieSchema);
+
+module.exports = Cookie;
